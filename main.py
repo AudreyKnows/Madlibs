0acb5f2eb23a990074bab10279f6f950e05fab31
AudreyKnows <124590776+AudreyKnows@users.noreply.github.com>
2023-02-06 14:32:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import time
+
+LinkedIn = "LinkedIn: www.linkedin.com/in/audrey-galasso"
+
+print("Follow Me on " + LinkedIn)
+time.sleep(5)
+
+#print(f"Follow Me on LinkedIn{LinkedIn}")
+
+print("'If you want something you have never had, you must be willing to do something you have never done.'")
+
+time.sleep(3)
+
+adj = input("adjectives:")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+
+madlib = f"Never thought i would be doing this, and actually enjoy it! Computer programming is {adj}! It takes me out of my comfort zone and makes me excited about my career and my family's future. I love to {verb1} salted caramel apple pies, under apple trees and {verb2} books in sandcastles. Thank your for Checking my Code! Stay tuned for my next project!"
+print(madlib)
+time.sleep(4)
+
+print("Audrey Galasso - AWS Certified Solutions Architect Associate & Certified ScrumMaster")
+
+
+            
+
+
+
+
+
